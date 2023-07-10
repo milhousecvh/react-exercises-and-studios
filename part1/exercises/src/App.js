655666534877a90185a412c3e24e609a6cfc2059
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-07-10 08:29:17 -0500

    components paired up in app

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -7,9 +7,12 @@ import BookList from './components/BookList.js';
 function App() {
   return (
     <div className="App">
-      <MovieList />
       <ChoresList />
+      <div className="similarComponents">
+      <MovieList />
       <BookList />
+      <HobbyLinks />
+      </div>
     </div>
   );
 }
