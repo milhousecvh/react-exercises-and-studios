3ebfcb0c21e20b361f99b05c60036818cb30e430
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-07-24 18:38:38 -0500

    finished exercises

--- part3/exercises/src/App.js
+++ part3/exercises/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import HobbyIntroduction from './components/Introduction';
+import MyPlants from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <h1>My Hobby: Plants</h1>
       <HobbyIntroduction />
+      <MyPlants />
     </div>
   );
 }
--- part3/exercises/src/components/Projects.js
+++ part3/exercises/src/components/Projects.js
@@ -1,13 +1,13 @@
 import data from './../data.json';
 import {useState} from 'react';
 
-export default MyPlants() {
+export default function MyProjects(){
     const [index, setIndex] = useState(0);
-    let plants = data.plants;
-    let plant = plants[index];
+    let projects = data.projects;
+    let project = projects[index];
 
     function handleClick() {
-        if (index < data.plants.length-1)
+        if (index < data.projects.length-1)
         {
             setIndex(index + 1);
         }
@@ -17,19 +17,12 @@ export default MyPlants() {
         }
     }
 
-function flowering() {
-    if (plant.flowers === true) {
-        return "a blooming plant";
-    } else {
-        return "does not bloom";
-    }
-}
 
     return(
         <div>
         <button onClick={handleClick}>Next</button>
-        <h3>{project.commonName} aka {project.scientificName}</h3>
-        <h3><flowering /></h3>
+        <h3>{project.commonName} aka {project.scientificName}
+        <br />This is a {project.flowers} plant.</h3>
         <img src={project.photoUrl} alt={project.commonName} />
         </div>
     );
