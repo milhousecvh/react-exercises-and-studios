dcef6e4684baa458a6c3a5a1b58611accf6af6bf
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-07-24 13:30:48 -0500

    step 14

--- part3/exercises/src/components/Projects.js
+++ part3/exercises/src/components/Projects.js
@@ -3,8 +3,8 @@ import {useState} from 'react';
 
 export default MyProjects() {
     const [index, setIndex] = useState(0);
-    let list = data.json;
-    let plantChoice = list[index];
+    let projects = data.projects;
+    let project = projects[index];
 
     function handleClick() {
         if (index < data.projects.length-1)
@@ -17,10 +17,20 @@ export default MyProjects() {
         }
     }
 
+function flowering(project) {
+    if ({project.flowers} === true) {
+        return a blooming plant";
+    } else {
+        return "does not bloom";
+    }
+}
+
     return(
         <div>
         <button onClick={handleClick}>Next</button>
-
+        <h3>{project.commonName} aka {project.scientificName}</h3>
+        <h3><flowering /></h3>
+        <img src={project.photoUrl} alt={project.commonName} />
         </div>
     );
 }
